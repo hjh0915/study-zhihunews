a7e19cc09f5d57102dc6ebbab33d3b37c30b6a8c
hjh0915 <623299157@qq.com>
2020-01-12 11:08:06 +0800

    增加评论和日期内容

--- app.py
+++ app.py
@@ -5,17 +5,16 @@ header = {
     'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'
 }
 
-def get_zhihu_news():
+def get_zhihu_news() -> str:
     """获取网站新闻内容"""
 
     url = "http://news-at.zhihu.com/api/4/news/latest"
     results = requests.get(url, headers=header)
-    data_str = json.dumps(results.json(), ensure_ascii=False)
-    data = json.loads(data_str)
+    data = json.dumps(results.json(), ensure_ascii=False)
 
     return data
 
-def get_zhihu_news_id(nid):
+def get_zhihu_news_by_id(nid: str):
     """根据id获取相应的新闻具体内容"""
 
     url = "http://news-at.zhihu.com/api/4/news/" + nid
@@ -24,3 +23,21 @@ def get_zhihu_news_id(nid):
     data = json.dumps(results.json(), ensure_ascii=False)
 
     return data
+
+def get_zhihu_comments_by_news_id(nid: str) -> str:
+    """根据id获取所有评论内容"""
+
+    url = "http://news-at.zhihu.com/api/4/story/" + nid + "/comments"
+    results = requests.get(url, headers=header)
+    comments = json.dumps(results.json(), ensure_ascii=False)
+
+    return comments
+
+def get_zhihu_before_news_by_date(date: str) -> str:
+    """根据id获取前一天的新闻内容"""
+
+    url = "http://news-at.zhihu.com/api/4/news/before/" + date 
+    results = requests.get(url, headers=header)
+    before_news = json.dumps(results.json(), ensure_ascii=False)
+
+    return before_news
--- tests/test_app.py
+++ tests/test_app.py
@@ -13,15 +13,28 @@ class TestAPP(unittest.TestCase):
         """获取知乎新闻内容"""
 
         data = app.get_zhihu_news()
-        for x in data['stories']:
-            print(x)
+        print(data)
 
-    def test_zhihu_news_id(self):
+    def test_zhihu_news_by_id(self):
         """根据id获取相应的新闻具体内容"""
 
         nid = '9718885'
-        data = app.get_zhihu_news_id(nid)
-        # print(data)
+        data = app.get_zhihu_news_by_id(nid)
+        print(data)
+
+    def test_zhihu_comments_by_news_id(self):
+        """根据id获取所有评论内容"""
+
+        nid = '9718898'
+        comments = app.get_zhihu_comments_by_news_id(nid)
+        print(comments)
+
+    def test_zhihu_before_news_by_date(self):
+        """根据id获取前一天的新闻内容"""
+
+        date = '20200112'
+        before_news = app.get_zhihu_before_news_by_date(date)
+        print(before_news)
 
     
 if __name__ == '__main__':
